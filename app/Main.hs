4d11fe28c9bf5559198b1f170a4a97814bc68497
William Zhu <Liam.Zhu@protonmail.com>
2022-08-08 21:18:04 -0400

    Currently at 90-100% on dealer hand producer, 80-100% on stand evaluator, 80-100% on action decider, 0% on outputter. The action decider still hasn't passed enough tests to my satisfaction.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import TTI
+import DataDeclarations
 
 main :: IO ()
-main = (print $ evaluateHitVsStand ([Two, Two], [Tens])) >> getChar >> pure ()
+main = (print $ evaluateHitVsStand ([Ace, Tens], [Tens])) >> getChar >> pure ()
+
--- app/TTI.hs
+++ app/TTI.hs
@@ -1,168 +1,136 @@
-{-# LANGUAGE LambdaCase, PatternSynonyms, BangPatterns #-}
+{-# LANGUAGE LambdaCase #-}
 module TTI where
+
+import DataDeclarations
+    ( Suggestion,
+      ExpectedValue,
+      Probability,
+      DealerCards,
+      CardsInPlay,
+      Card(ReducedAce, Two, Ten_Jack_Queen_King, Ace),
+      Action(Stand, Surrender, DoubleAction, Hit, Split) )
+
+import CommonNamesAndFunctions
+    ( allRanks,
+      natural,
+      safeTailThroughNil,
+      probabilityTenJackQueenKing,
+      probabilityOther,
+      appendNewCardPlayer,
+      probabilityOfPlayerDraw, safeInitThroughNil )
+      
+import CardValueChecker ( cardsToValue, valueCheck )
+
+import DealerHands
+    ( dealerHandsNotSix,
+      dealerHands21,
+      dealerHandsNatural,
+      dealerHandsSix )
 
 import Data.List (intersect, isPrefixOf, (\\))
 import Control.Applicative ((<**>))
 
---Basic types, type synonyms.
-
-data Action =
-
-      Split
-    | DoubleAction
-    | Hit
-    | Stand
-
-    deriving (Eq, Ord, Show)
-
-data Card =
-
-      ReducedAce
-    | Two
-    | Three
-    | Four
-    | Five
-    | Six
-    | Seven
-    | Eight
-    | Nine
-    | Ten_Jack_Queen_King
-    | Ace
-    
-    deriving (Eq, Enum, Show)
-
--- Pattern synonym for Ten_Jack_Queen_King added for debugging purposes.
-
-pattern Tens :: Card
-pattern Tens = Ten_Jack_Queen_King
-
-type CardsInPlay = ( PlayerCards , DealerCards )
-
-type PlayerCards = [ Card ]
-
-type DealerCards = [ Card ]
-
-type CardsRevealed = [ Card ]
-
-type Players = [ Card ]
-
-type Probability = Double
-
-type ExpectedValue = Double
-
-type Suggestion = (ExpectedValue, Action)
-
-
-
--- | A list of cards that comprise a rank, or all cards of
--- the same suit. Note absence of ReducedAce.
-
-allRanks :: [Card]
-allRanks = [ Two .. Ace ]
-
-
-
--- | Hands that are "naturals"
-
-natural :: [[Card]]
-natural = [ [ Ace , Ten_Jack_Queen_King ] , [ Ten_Jack_Queen_King , Ace ] ]
-
-
-
--- Some safe versions of list functions, written with nil as a default value
-
-
-
-safeTailThroughNil :: [a] -> [a]
-safeTailThroughNil = \case
-
-    [] -> []
-    x:xs -> xs
-
-
-
-safeInitThroughNil :: [a] -> [a]
-safeInitThroughNil = \case
-
-    [] -> []
-    [x] -> []
-    x:xs -> x : safeInitThroughNil xs
-
-
-
-safeLastThroughNil :: [a] -> [a]
-safeLastThroughNil = \case
-
-    [] -> []
-    [x] -> [x]
-    x:xs -> safeLastThroughNil xs
-
-
-
-  -- Used both in dealer and player sides, easier to move this out of a where block.
 
 
 
 
+-- Evaluation functions that provide support for the top-level, wherein more than the usual
+-- split vs hit options exist.
 
-numberOfCardsIn :: Card -> [Card] -> Double
-numberOfCardsIn specificCard =
+
+
+
+evaluateInitial :: CardsInPlay -> Suggestion
+evaluateInitial cardsInPlay = case cardsInPlay of
       
-    fromIntegral . length . intersect [specificCard]
+        ( [firstCard , secondCard], _ ) | firstCard == secondCard ->
+            
+            maximum $ optionsWithSplit cardsInPlay
+
+        _ -> maximum $ optionsWithoutSplit cardsInPlay
 
 
 
-probabilityTenJackQueenKing :: [Card] -> Double
-probabilityTenJackQueenKing cardsInPlay =
-
-    ( 128 - numberOfCardsIn Ten_Jack_Queen_King cardsInPlay ) /
-    (416 - fromIntegral (length cardsInPlay) )
-
-
-
-probabilityOther :: [Card] -> Card -> Double
-probabilityOther cardsInPlay other =
-
-    ( 32 - numberOfCardsIn other cardsInPlay ) /
-    (416 - fromIntegral (length cardsInPlay) )
+optionsWithSplit :: CardsInPlay -> [Suggestion]
+optionsWithSplit cardsInPlay =
+  
+  [ doubleCards cardsInPlay , undefined ,
+  surrender , evaluateHitVsStand cardsInPlay ]
 
 
-{-
-judgeInitial :: CardsInPlay -> Probability
-judgeInitial cards = maximum [ doubleCards , splitCards , surrender , ( hit cards ) , ( stand cards ) ]
+
+optionsWithoutSplit :: CardsInPlay -> [Suggestion]
+optionsWithoutSplit cardsInPlay =
+  
+    [ doubleCards cardsInPlay , surrender , evaluateHitVsStand cardsInPlay]
+
+
+
+surrender :: Suggestion
+surrender = ( 0.50 , Surrender )
+
+
+
+doubleCards :: CardsInPlay -> Suggestion
+doubleCards cardsInPlay = 
+      
+    ( (subtract 1) . (*2) . sum $
+    deriveCorrectProbabilityForDouble <$>
+    appendNewCardPlayer cardsInPlay
+    , DoubleAction )
+
   where
-    surrender = 0.50
 
-stand :: CardsInPlay -> Probability
-stand cards = calcDealer cards undefined
+    deriveCorrectProbabilityForDouble newCardsInPlay =
 
-hit = undefined
-
-splitCards = undefined
-
-doubleCards = undefined-}
+      probabilityOfPlayerDraw newCardsInPlay *
+      calculateStandEV newCardsInPlay
 
 
-{- Anna Kournikova! Might be broken, might not be broken, but is taking 33gb-1.5gb of ram to run. -}
+
+splitCards :: CardsInPlay -> [Suggestion]
+splitCards (playerCards , dealerFaceUp) = 
+    
+    maximum $ splitAppender <$> appendNewCardPlayer (safeInitThroughNil 
+    playerCards , dealerFaceUp)
+
+  where
+
+    splitAppender cardsInPlay =
+      
+        (\(ev,action) -> ((probabilityOfPlayerDraw cardsInPlay) * ev , Split)) <$>
+         optionsWithoutSplit cardsInPlay
+
+
+
+
+
+
+{- Terrible performance here, with 8 minute evaluate times on O2.
+Needs testing, but seems correct when compared to 
+http://www.bjstrat.net/cgi-bin/cdca.cgi . -}
 
 evaluateHitVsStand :: CardsInPlay -> Suggestion
 evaluateHitVsStand cardsInPlay =
 
-    evaluateHits (calculateStands cardsInPlay) $
+    calculateStands cardsInPlay `evaluateHits`
     correctProbabilityCardsInPlay <$>
     appendNewCardPlayer cardsInPlay
 
   where
+
+    
     
     correctProbabilityCardsInPlay :: CardsInPlay -> Suggestion
     correctProbabilityCardsInPlay cardsInPlayNew =
 
-      ( probabilityOfPlayerDraw cardsInPlayNew *
-      ( fst . evaluateHitVsStand $ cardsInPlayNew )
-      , Hit )
+        ( probabilityOfPlayerDraw cardsInPlayNew *
+        ( fst $ evaluateHitVsStand cardsInPlayNew )
+        , Hit )
 
 
 
+infixl 0 `evaluateHits`
 evaluateHits :: Suggestion -> [Suggestion] -> Suggestion
 evaluateHits stand [] = stand
 evaluateHits stand hit = max stand (sum $ fst <$> hit, Hit)
@@ -171,27 +139,6 @@ evaluateHits stand hit = max stand (sum $ fst <$> hit, Hit)
 
 calculateStands :: CardsInPlay -> Suggestion
 calculateStands cardsInPlay = (,) (calculateStandEV cardsInPlay) Stand
-
-
-
-appendNewCardPlayer :: CardsInPlay -> [ CardsInPlay ]
-appendNewCardPlayer cardsInPlay@( playerCards , dealerFaceUp ) =
-  
-    fmap (flip (,) dealerFaceUp) . filter (valueCheck 21 (>=)) $
-    (pure playerCards) <**> (flip (++) <$> pure <$> allRanks )
-
-
-
-probabilityOfPlayerDraw :: CardsInPlay -> Probability
-probabilityOfPlayerDraw cardsInPlay@(playerCards, dealerCards) =
-  
-    let combinedCardsInPlay = safeInitThroughNil playerCards ++ dealerCards in
-    
-    case safeLastThroughNil playerCards of
-
-        [] -> 1
-        [Ten_Jack_Queen_King] -> probabilityTenJackQueenKing combinedCardsInPlay
-        [otherCard] -> probabilityOther combinedCardsInPlay otherCard
 
 
 
@@ -253,10 +200,12 @@ For naturals:
 
 
 
+    playerHandValue :: Int
     playerHandValue = cardsToValue playerCards
 
 
-
+    
+    playerSixCardCharlieCalc :: ExpectedValue
     playerSixCardCharlieCalc = 
       
         2 - 2 * calcDealer cardsInPlay
@@ -273,6 +222,7 @@ For naturals:
 
 
 
+    playerNonNatural21Calc :: ExpectedValue
     playerNonNatural21Calc =
 
         2 - 2 * calcDealer cardsInPlay
@@ -289,6 +239,7 @@ For naturals:
 
 
 
+    playerNormalCalc :: ExpectedValue
     playerNormalCalc =
 
           2 - 2 * calcDealer cardsInPlay
@@ -321,145 +272,3 @@ probabilityOfDealerHand cardsInPlay dealerCards = case dealerCards of
         probabilityOfDealerHand (Ten_Jack_Queen_King:cardsInPlay) xs
     otherCard:xs -> probabilityOther cardsInPlay otherCard *
         probabilityOfDealerHand (otherCard:cardsInPlay) xs
-
-
-
-
-
---Dealer hands aliases for non-Six-Card-Charlie. Also useful for testing.
-
-
-
--- | Generally all dealerHands that don't trigger Six-Card-Charlie.
-
-dealerHandsNotSix :: [DealerCards]
-dealerHandsNotSix = filter ( (/= 6 ) . length ) $ dealerHands
-
-
-
--- Specific, individual subsets of dealerHands that don't have Six-Card-Charlie
-
-dealerHands21 :: [DealerCards]
-dealerHands21 = filter (valueCheck 21 (==) ) dealerHandsNotSix
-
-dealerHands20 :: [DealerCards]
-dealerHands20 = filter (valueCheck 20 (==) ) dealerHandsNotSix
-
-dealerHands19 :: [DealerCards]
-dealerHands19 = filter (valueCheck 19 (==) ) dealerHandsNotSix
-
-dealerHands18 :: [DealerCards]
-dealerHands18 = filter (valueCheck 18 (==) ) dealerHandsNotSix
-
-dealerHands17 :: [DealerCards]
-dealerHands17 = filter (valueCheck 17 (==) ) dealerHandsNotSix
-
-dealerHandsNatural :: [DealerCards]
-dealerHandsNatural = natural
-
-
-
---Dealer Six-Card-Charlie hands
-
--- | Dealer Six-Card-Charlie hands.
-
-dealerHandsSix :: [DealerCards]
-dealerHandsSix = filter ( (==6) . length ) dealerHands
-
-
-
-{- The subsequent functions generate the term "dealerHands", which contains all possible dealer hands. -}
-
-
-
--- | Dealer hands. Iterate creates a list on which elements of the list are repetitions
--- of the action x times, where x is the index of the list starting from 0.
-
-dealerHands :: [DealerCards]
-dealerHands = iterate appendNewCardDealer (pure <$> allRanks) !! 5
-
-
-
--- | Appends a new card to a set of cards, but starts by splitting
--- the existing deck into the parts where a dealer won't hit
--- (under stand on soft 17) and parts where a dealer would hit,
--- "filter valueCheck 17"
-
-appendNewCardDealer :: [[Card]] -> [[Card]]
-appendNewCardDealer preExistingCards =
-
-    let dealerStandCards = filter ( valueCheck 17 (<=) ) preExistingCards
-        dealerHitCards =
-          
-          filter ( valueCheck 21 (>=) ) $
-          ( filter ( valueCheck 17 (>) ) preExistingCards ) <**>
-          (flip (++) <$> pure <$> allRanks) in
-
-          --Last value filters preExistingCards based on player hitting,
-          --starting at the end. The ((:) <$> deck) <*> adds cards, then
-          --the filter before it removes combinations that would cause the
-          --dealer to go bust.
-
-    dealerStandCards <> dealerHitCards
-
-
-    
--- | Mostly avoids a direct call to cardsToValue, otherwise mostly identical to directly
--- calling the Boolean. Note the invisible [Card] via eta reduction.
-
-valueCheck :: Int -> (Int -> Int -> Bool) -> [Card] -> Bool
-valueCheck value boolType = boolType value . cardsToValue 
-
-
-
-
-
--- | The next four functions, focusing on cardsToValue, derive the hand value.
--- Note the mutual recursion involved; cardsToValue calls checkForAces, checkForAces
--- calls cardsToValue. This strips aces, one by one, as necessary, to attempt to push
--- the value below 21. If it still fails, checkForAces triggers 22 and breaks out of the
--- recursive loop.
--- Note that ReducedAce never actually appears in a hand, only as an item existing within
--- the card value calculation system.
-
-cardsToValue :: [Card] -> Int
-cardsToValue cards =
-  
-    if basicValueOfCards cards > 21
-        then checkForAces cards
-        else basicValueOfCards cards
-  
-
-
--- | Note fromEnum requires (+1) due to the absence of a zero value.
-
-basicValueOfCards :: [Card] -> Int
-basicValueOfCards = foldr ( (+) . (+1) . fromEnum ) (0)
-
-
-
--- | As mentioned above, checkForAces is in a mutually-recursive loop with cardsToValue,
--- breaking out via a declaration of 22. It calls reduceAce to reduce aces one by one from
--- 11 to 1 in value. Using 22 to indicate bust is admittedly a bit inelegant.
-
-
-
-checkForAces :: [Card] -> Int
-checkForAces cards =
-  
-    if elem Ace cards
-        then cardsToValue $ reduceAce cards
-        else 22
-
-
-  
--- | As before, note that reduceAce never actually creates a ReducedAce in a hand, only
--- for cardsToValue, checkForAces, etc, for calculation purposes. Note that this only reduces
--- one ace. Other such functions might reduce all aces.
-
-reduceAce :: [Card] -> [Card]
-reduceAce hand = case hand of
-
-    [] -> []
-    Ace : xs -> ReducedAce:xs
-    x : xs -> x : reduceAce xs
